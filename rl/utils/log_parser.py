506dbd71ce1676f59bae4df1650f59e10cda86c1
AlexKaravaev <karavaev-sasha@mail.ru>
2020-05-22 11:49:42 +0300

    Update log parser

--- rl/utils/log_parser.py
+++ rl/utils/log_parser.py
@@ -5,15 +5,18 @@ import plotly.graph_objs as go
 import pandas as pd
 import scipy.signal
 import seaborn as sns; sns.set()
-
+import numpy as np
 import matplotlib.pyplot as plt
 from tbparser.summary_reader import SummaryReader
+import matplotlib.patches as mpatches
+
+
 
 
 def smooth_and_plot(col_name, metric_df):
 
     smoothed_df = metric_df.copy()
-    smoothed_df[col_name] = scipy.signal.savgol_filter(metric_df[col_name], 101,3)
+    smoothed_df[col_name] = scipy.signal.savgol_filter(metric_df[col_name], 201,3)
 
     lower_df = smoothed_df.copy()
     lower_df[col_name] = lower_df[col_name] - metric_df[col_name].std()
@@ -28,12 +31,12 @@ def smooth_and_plot(col_name, metric_df):
     
     fig, ax = plt.subplots(1)
     ax.plot(smoothed_df['step'], smoothed_df[col_name], lw=2, label='smoothed_reward', color='blue')
-    ax.fill_between(lower_df['step'], lower_df[col_name], upper_df[col_name], facecolor='yellow', alpha=0.79,
+    ax.fill_between(lower_df['step'], lower_df[col_name], upper_df[col_name], facecolor='blue', alpha=0.3,
                     label='1 std range')
     ax.legend(loc='upper left')
     ax.set_ylabel(col_name)
     ax.set_xlabel('step')
-    ax.grid()
+    ax.grid(True)
     plt.show()
 
 if __name__=="__main__":
@@ -47,17 +50,14 @@ if __name__=="__main__":
     closs_dict = []
     aloss_dict = []
     for item in reader:
-        if item.tag == 'reward':
-            reward_dict.append({"step":item.step,"reward":item.value})
-        if item.tag == "critic_loss":
-            closs_dict.append({"step":item.step,"critic_loss":item.value})
-        if item.tag == "actor_loss":
-            aloss_dict.append({"step":item.step,"actor_loss":item.value})
+        if item.tag == 'episode_reward':
+            reward_dict.append({"step":item.step,"episode_reward":item.value})
+            rewards.append(item.value)
+
     print("Finished reading")
     reward_df = pd.DataFrame.from_dict(reward_dict)
     closs_df = pd.DataFrame.from_dict(closs_dict)
     aloss_df = pd.DataFrame.from_dict(aloss_dict)
-
-    smooth_and_plot('reward', reward_df)
-    smooth_and_plot('critic_loss', closs_df)
-    smooth_and_plot('actor_loss', aloss_df)
+    smooth_and_plot('episode_reward', reward_df)
+    #smooth_and_plot('critic_loss', closs_df)
+    #smooth_and_plot('actor_loss', aloss_df)
